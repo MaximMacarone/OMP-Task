dab5cd51e60d474726ba9ffaa8ca225132533d9b
Maxim Makarenkov <maximmacarone@gmail.com>
2024-10-01 03:09:26 +0300

    Simple client app

---
+++ Client/client-app.cpp
@@ -0,0 +1,46 @@
+#include <sdbus-c++/sdbus-c++.h>
+#include "../Task-1/com.system.permissions/permissions-client-impl.h"
+#include "../Task-2/com.system.time/time-client-impl.h"
+#include <iostream>
+#include <unistd.h>
+
+void getPermission();
+void getSystemTime();
+
+int main() {
+    while(true) {
+        int selection;
+        std::cout << "1.Request check system time permission\n";
+        std::cout << "2.Get system time\n";
+        std::cin >> selection;
+
+        switch (selection)
+        {
+        case 1:
+            getPermission();
+            break;
+        case 2:
+            getSystemTime();
+        default:
+            break;
+        }
+    }
+}
+
+void getPermission() {
+    sdbus::ServiceName destination{"com.system.permissions"};
+    sdbus::ObjectPath objectPath{"/com/system/permissions"};
+    
+    PermissionProxy permissionProxy(std::move(destination), std::move(objectPath));
+    permissionProxy.RequestPermission(0);
+    sleep(1);
+}
+
+void getSystemTime() {
+    sdbus::ServiceName destination{"com.system.time"};
+    sdbus::ObjectPath objectPath{"/com/system/time"};
+
+    TimeProxy timeProxy(std::move(destination), std::move(objectPath));
+    timeProxy.GetSystemTime();
+    sleep(1);
+}
--- Task-2/com.system.time/time-client.cpp
+++
@@ -1,9 +0,0 @@
-#include "time-client-impl.h"
-
-int main() {
-    sdbus::ServiceName destination{"com.system.time"};
-    sdbus::ObjectPath objectPath{"/com/system/time"};
-    
-    TimeProxy timeProxy(std::move(destination), std::move(objectPath));
-    timeProxy.GetSystemTime();
-}
